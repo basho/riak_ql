67dc280c2f8c4c768a2e857e012905dcde2843a8
Andrei Zavada <johnhommer@gmail.com>
2015-12-15 17:40:56 +0000

    WIP

--- src/riak_ql_window_agg_fns.erl
+++ src/riak_ql_window_agg_fns.erl
@@ -3,7 +3,7 @@
 %% riak_ql_window_agg_fns: implementation of Windows Aggregation Fns
 %%                         for the query runner
 %%
-%% Copyright (c) 2007-2015 Basho Technologies, Inc.  All Rights Reserved.
+%% Copyright (c) 2015 Basho Technologies, Inc.  All Rights Reserved.
 %%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file
@@ -24,8 +24,10 @@
 
 -export([
          get_type_sig/1,
-         get_starting_acc/1,
-         finalise/2,
+         group_fn_starting_acc/2,
+         group_fn_start/3,
+         group_fn_continue/3,
+         group_fn_finalise/2,
          '+'/2,
          '-'/2,
          '/'/2,
@@ -33,6 +35,15 @@
          'AVG'/3
          ]).
 
+-type operator() :: '+' | '-' | '*' | '/'.
+-type group_function() :: 'COUNT' | 'SUM' | 'AVG' | 'MIN' | 'MAX' | 'STDEV' |
+                          %% and a few more involving more than a simple traversal
+                          'MODE' | 'MEDIAN'.
+
+-include("riak_ql_ddl.hrl").
+-include_lib("riak_pb/include/riak_pb_kv_codec.hrl").  %% for UINT_MAX etc
+
+%% functions used in expression type validation
 get_type_sig('+')   -> [
                         {sint64, sint64},
                         {double, double}
@@ -42,7 +53,7 @@ get_type_sig('-')   -> [
                         {double, double}
                        ];
 get_type_sig('/')   -> [
-                        {sint64, double},
+                        {sint64, sint64},
                         {double, double}
                        ];
 get_type_sig('*')   -> [
@@ -62,9 +73,51 @@ get_type_sig('AVG') -> [
 
 '*'(A, B) -> A * B.
 
-get_starting_acc('AVG') -> {0, 0}.
+-spec starting_state(field_type(), group_function()) ->
+                            {StartingValue::number(), N::non_neg_integer()} |
+                            number().
+starting_state(_, 'COUNT') ->
+    0;
+starting_state(Type, Fun)
+  when Fun == 'SUM';
+       Fun == 'AVG';
+       Fun == 'MODE';
+       Fun == 'MEDIAN' ->
+    zero_of_type(Type);
+starting_state(_, Fun)
+  when Fun == 'MIN';
+       Fun == 'MAX' ->
+    not_a_value;
+starting_state(_, 'STDEV') ->
+    {0, 0.0, 0.0}.
 
-finalise('AVG', {Total, No}) -> Total/No.
+
+
+%% Group functions (avg, mean etc). These can only appear as top-level
+%% expressions in SELECT part, and there can be only one in a query.
+%% Can take an Expr that includes the column identifier and some static
+%% values.
+%%
+%% Incrementally operates on chunks, needs to carry state.
+group_fn_run('COUNT', _Type, _Expr, {identifier, _ColName}, _Rec, Acc) ->
+    fun(_V, Acc) ->
+            Acc + 1
+    end;
+group_fn_run() ->
+
+
+group_fn_finish('AVG', _Type, {N, Acc}) ->
+    Acc / N;
+group_fn_finish('SUM', _Type, Acc) ->
+    Acc;
+group_fn_finish('MIN', _Type, Acc) ->
+    Acc;
+group_fn_finish('MAX', _Type, Acc) ->
+    Acc;
+group_fn_finish('STDEV', _Type, {N, SumOfSquares, Mean}) ->
+    %% TODO: rework using special formulae to prevent overflows
+    math:sqrt(SumOfSquares / N - Mean * Mean).
+
 
 'AVG'(RObjs, Expr, Acc) when is_list(RObjs) ->
     FoldFun = fun(Rec, {Total, No}) when is_list(Rec) ->
@@ -76,6 +129,3 @@ finalise('AVG', {Total, No}) -> Total/No.
 eval({identifier, ColName}, Rec) ->
     {ColName, V} = lists:keyfind(ColName, 1, Rec),
     V;
-eval(Expr, RObj) ->
-    io:format("Expr is ~p~n RObj is ~p~n", [Expr, RObj]),
-    12345.
