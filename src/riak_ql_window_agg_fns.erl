1ff8f550e1127dea4211cc2fafcd941c179516e7
Andrei Zavada <johnhommer@gmail.com>
2015-12-16 16:01:04 +0000

    rework to be usable in riak_kv_qry_worker
    * only window functions here (scalar operators dropped);
    * mean and median stubbed;
    * stdev needs a better implementation to deal with precision loss.

--- src/riak_ql_window_agg_fns.erl
+++ src/riak_ql_window_agg_fns.erl
@@ -24,18 +24,11 @@
 
 -export([
          get_type_sig/1,
-         group_fn_starting_acc/2,
-         group_fn_start/3,
-         group_fn_continue/3,
-         group_fn_finalise/2,
-         '+'/2,
-         '-'/2,
-         '/'/2,
-         '*'/2,
-         'AVG'/3
-         ]).
+         get_starting_state/2,
+         finalise/2,
+         'COUNT'/2, 'SUM'/2, 'AVG'/2, 'MIN'/2, 'MAX'/2, 'STDEV'/2, 'MODE'/2, 'MEDIAN'/2
+        ]).
 
--type operator() :: '+' | '-' | '*' | '/'.
 -type group_function() :: 'COUNT' | 'SUM' | 'AVG' | 'MIN' | 'MAX' | 'STDEV' |
                           %% and a few more involving more than a simple traversal
                           'MODE' | 'MEDIAN'.
@@ -44,53 +37,57 @@
 -include_lib("riak_pb/include/riak_pb_kv_codec.hrl").  %% for UINT_MAX etc
 
 %% functions used in expression type validation
-get_type_sig('+')   -> [
-                        {sint64, sint64},
-                        {double, double}
-                       ];
-get_type_sig('-')   -> [
-                        {sint64, sint64},
-                        {double, double}
-                       ];
-get_type_sig('/')   -> [
-                        {sint64, sint64},
-                        {double, double}
-                       ];
-get_type_sig('*')   -> [
-                        {sint64, sint64},
-                        {double, double}
-                       ];
-get_type_sig('AVG') -> [
-                        {sint64, double},
-                        {double, double}
-                       ].
+%% get_type_sig('+')   -> [
+%%                         {sint64, sint64},
+%%                         {double, double}
+%%                        ];
+%% get_type_sig('-')   -> [
+%%                         {sint64, sint64},
+%%                         {double, double}
+%%                        ];
+%% get_type_sig('/')   -> [
+%%                         {sint64, sint64},
+%%                         {double, double}
+%%                        ];
+%% get_type_sig('*')   -> [
+%%                         {sint64, sint64},
+%%                         {double, double}
+%%                        ];
+get_type_sig('COUNT' ) -> [{sint64, sint64}, {double, sint64}];
+get_type_sig('SUM'   ) -> [{sint64, sint64}, {double, double}];
+get_type_sig('MIN'   ) -> [{sint64, sint64}, {double, double}];
+get_type_sig('MAX'   ) -> [{sint64, sint64}, {double, double}];
+get_type_sig('MODE'  ) -> [{sint64, sint64}, {double, double}];
+get_type_sig('MEDIAN') -> [{sint64, sint64}, {double, double}];
+get_type_sig('AVG'   ) -> [{sint64, double}, {double, double}];  %% double promotion
+get_type_sig('STDEV' ) -> [{sint64, double}, {double, double}].  %% ditto
 
-'+'(A, B) -> A + B.
+%% '+'(A, B) -> A + B.
+%% '-'(A, B) -> A - B.
+%% '/'(A, B ) -> A / B.
+%% '*'(A, B) -> A * B.
 
-'-'(A, B) -> A - B.
-
-'/'(A, B ) -> A / B.
-
-'*'(A, B) -> A * B.
-
--spec starting_state(field_type(), group_function()) ->
-                            {StartingValue::number(), N::non_neg_integer()} |
-                            number().
-starting_state(_, 'COUNT') ->
+-spec get_starting_state(field_type(), group_function()) ->
+                                tuple() | number().
+get_starting_state(_, 'COUNT') ->
     0;
-starting_state(Type, Fun)
+get_starting_state(Type, Fun)
   when Fun == 'SUM';
        Fun == 'AVG';
        Fun == 'MODE';
        Fun == 'MEDIAN' ->
-    zero_of_type(Type);
-starting_state(_, Fun)
+    zero_of_type_with_counter(Type);
+get_starting_state(_Type, Fun)
+  when Fun == 'AVG' ->
+    zero_of_type_with_counter(double);
+get_starting_state(_, Fun)
   when Fun == 'MIN';
        Fun == 'MAX' ->
     not_a_value;
-starting_state(_, 'STDEV') ->
+get_starting_state(_, 'STDEV') ->
     {0, 0.0, 0.0}.
-
+zero_of_type_with_counter(sint64) -> {0, 0};
+zero_of_type_with_counter(double) -> {0, 0.0}.
 
 
 %% Group functions (avg, mean etc). These can only appear as top-level
@@ -99,33 +96,61 @@ starting_state(_, 'STDEV') ->
 %% values.
 %%
 %% Incrementally operates on chunks, needs to carry state.
-group_fn_run('COUNT', _Type, _Expr, {identifier, _ColName}, _Rec, Acc) ->
-    fun(_V, Acc) ->
-            Acc + 1
-    end;
-group_fn_run() ->
 
 
-group_fn_finish('AVG', _Type, {N, Acc}) ->
+'COUNT'(_Arg, _State = N) ->
+    N + 1.
+
+'SUM'  (Arg, _State = Total) ->
+    Arg + Total.
+
+'AVG'  (Arg, _State = {N, Acc}) ->
+    {N + 1, Acc + Arg}.
+
+'MIN'  (Arg, _State = Min) ->
+    if Min == not_a_value ->
+            Min;
+       Arg < Min ->
+            Arg;
+       el/=se ->
+            Min
+    end.
+
+'MAX'  (Arg, _State = Max) ->
+    if Max == not_a_value ->
+            Max;
+       Arg > Max ->
+            Arg;
+       el/=se ->
+            Max
+    end.
+
+'MODE'  (_Arg, _State) ->
+    %% stub
+    _State.
+
+'MEDIAN'(_Arg, _State) ->
+    %% stub
+    _State.
+
+'STDEV'(Arg, _State = {N, SumOfSquares, Mean}) ->
+    {N + 1, SumOfSquares + Arg * Arg, Mean + (Mean - Arg) / (N+1)}.
+
+
+finalise('COUNT', N) ->
+    N;
+finalise('SUM', Sum) ->
+    Sum;
+finalise('AVG', {N, Acc}) ->
     Acc / N;
-group_fn_finish('SUM', _Type, Acc) ->
+finalise('MIN', Acc) ->
     Acc;
-group_fn_finish('MIN', _Type, Acc) ->
+finalise('MAX', Acc) ->
     Acc;
-group_fn_finish('MAX', _Type, Acc) ->
+finalise('MODE', Acc) ->
     Acc;
-group_fn_finish('STDEV', _Type, {N, SumOfSquares, Mean}) ->
+finalise('MEDIAN', Acc) ->
+    Acc;
+finalise('STDEV', {N, SumOfSquares, Mean}) ->
     %% TODO: rework using special formulae to prevent overflows
-    math:sqrt(SumOfSquares / N - Mean * Mean).
-
-
-'AVG'(RObjs, Expr, Acc) when is_list(RObjs) ->
-    FoldFun = fun(Rec, {Total, No}) when is_list(Rec) ->
-                      V = eval(Expr, Rec),
-                      {Total + V, No + 1}
-              end,
-    lists:foldl(FoldFun, Acc, RObjs).
-
-eval({identifier, ColName}, Rec) ->
-    {ColName, V} = lists:keyfind(ColName, 1, Rec),
-    V;
+    math:sqrt(SumOfSquares / (N - Mean * Mean)).
