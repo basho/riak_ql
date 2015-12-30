ef9e394df4d3e9ecc8f244b1cde6ebc5e35ae9e1
Bryce Kerley <bkerley@brycekerley.net>
2015-12-30 11:26:11 -0500

    one more describe parse test

--- test/parser_describe_tests.erl
+++ test/parser_describe_tests.erl
@@ -14,3 +14,7 @@
 simple_describe_test() ->
     ?sql_comp_assert("describe GeoCheckins",
                      #riak_sql_describe_v1{'DESCRIBE' = <<"GeoCheckins">>}).
+
+uppercase_quoted_describe_test() ->
+    ?sql_comp_assert("DESCRIBE \"GeoCheckins\"",
+                     #riak_sql_describe_v1{'DESCRIBE' = <<"GeoCheckins">>}).
